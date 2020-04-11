f030d08127cb5bec088ca35318033b2832972e48
jonathan <jonathanvanderhout@gmail.com>
2020-04-10 22:54:08 -0400

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './appheader/appheader.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
+import { AppfooterComponent } from './appfooter/appfooter.component';
+import { AppsettingComponent } from './appsetting/appsetting.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
